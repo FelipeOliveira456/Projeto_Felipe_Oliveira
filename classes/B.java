f25818ffc23ec879949429607c9b87962bdb57c8
Felipe <felipe.oliveira.ger@gmail.com>
2024-10-26 17:34:38 -0300

    classes A.java e B.java adicionadas

---
+++ classes/B.java
@@ -0,0 +1,28 @@
+public class B {
+    private int B1;
+    private double B2;
+    
+    public void setA1(int B1){
+        this.B1 = B1;
+    }
+
+    public int getA1(){
+        return B1;
+    }
+
+    public void setA2(double B2){
+        this.B2 = B2;
+    }
+
+    public double getA2(){
+        return B2;
+    }
+
+    public void MB1(){
+        System.out.println("MB1");
+    }
+
+    public void MB2(){
+        System.out.println("MB2");
+    }
+}
